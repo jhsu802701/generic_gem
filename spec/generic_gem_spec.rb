eaa2fd519db7a27ee44f662467efcaa7ec99f6e1
Vagrant User <vagrant@vagrant-rvm>
2015-03-08 20:47:30 -0500

    Outlined tests

--- spec/generic_gem_spec.rb
+++ spec/generic_gem_spec.rb
@@ -8,4 +8,20 @@ describe GenericGem do
   it 'does something useful' do
     expect(false).to eq(true)
   end
+  
+  describe 'process' do
+    # Create new gem in tmp directory
+    # Make the changes central to this generic_gem Ruby gem
+    # Check $HOME/.bundle/config settings
+    # Check for code of conduct
+    # Check for MIT license
+    # Check for initial rspec tests
+    # Check that initial version number is 0.0.0
+    # Check that bin/setup and bin/console are executable
+    # Check the gem_test.sh script
+    # Check the gem_console.sh script
+    # Check the *_spec.rb file
+    
+  end
+  
 end
