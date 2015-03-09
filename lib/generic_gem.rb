9fe55afd7ee94301de6ebb88593c588433f11905
Vagrant User <vagrant@vagrant-rvm>
2015-03-08 22:52:19 -0500

    Initial version of new gem is 0.0.0

--- lib/generic_gem.rb
+++ lib/generic_gem.rb
@@ -1,22 +1,29 @@
 require "generic_gem/version"
+require "string_in_file"
 
 module GenericGem
   def self.create (gem_name)
     dir_main = File.expand_path("../../", __FILE__)
+    subdir_main = gem_name
     
-    t1 = Thread.new {
-      puts
-      puts "*************************************************"
-      puts "Setting up #{ENV['HOME']}/.bundle/config"
-      puts "Includes the Code of Conduct (CODE_OF_CONDUCT.md file) and MIT license (LICENSE.txt file)"
-      puts "Using rspec testing"
-      system ("cp #{dir_main}/lib/files_to_add/config #{ENV['HOME']}/.bundle/config")
+    puts
+    puts "*************************************************"
+    puts "Setting up #{ENV['HOME']}/.bundle/config"
+    puts "Includes the Code of Conduct (CODE_OF_CONDUCT.md file) and MIT license (LICENSE.txt file)"
+    puts "Using rspec testing"
+    system("cp #{dir_main}/lib/files_to_add/config #{ENV['HOME']}/.bundle/config")
       
-      puts "*************************************************"
+    t1 = Thread.new {  
+      puts "********************"
+      puts "Creating the new gem"
       puts "bundle gem #{gem_name}"
-      system ("bundle gem #{gem_name}")
-      
+      system("bundle gem #{gem_name}")  
       }
     t1.join
+    
+    puts "**********************"
+    puts "Initial version: 0.0.0"
+    StringInFile.replace("0.1.0", "0.0.0", "#{subdir_main}/lib/#{gem_name}/version.rb")
+    
   end
 end
--- spec/generic_gem_spec.rb
+++ spec/generic_gem_spec.rb
@@ -13,16 +13,16 @@ describe GenericGem do
     require 'string_in_file'
     system("rm -rf tmp")
     GenericGem.create("tmp")
-    #system("rm $HOME/.bundle/config")
-    # Create new gem in tmp directory
-    # Make the changes central to this generic_gem Ruby gem
-    # Check $HOME/.bundle/config settings
     it "Bundler has the proper configuration settings" do
-      puts "#{ENV['HOME']}/.bundle/config"
       expect(StringInFile.present("BUNDLE_GEM__COC: true", "#{ENV['HOME']}/.bundle/config")).to eq(true)
       expect(StringInFile.present("BUNDLE_GEM__MIT: true", "#{ENV['HOME']}/.bundle/config")).to eq(true)
       expect(StringInFile.present("BUNDLE_GEM__TEST", "#{ENV['HOME']}/.bundle/config")).to eq(true)
     end
+    
+    it "The new gem has the initial version number 0.0.0" do
+      expect(StringInFile.present("0.0.0", "tmp/lib/tmp/version.rb")).to eq(true)
+    end
+    
     # Check for code of conduct
     # Check for MIT license
     # Check for initial rspec tests
