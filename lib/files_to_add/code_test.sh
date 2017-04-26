f2ed0587e50a7206ffa570ca85c78a986c514531
Jason Hsu <rubygems@jasonhsu.com>
2017-04-26 20:51:02 +0000

    Added bundle exec to code_test.sh actions

--- lib/files_to_add/code_test.sh
+++ lib/files_to_add/code_test.sh
@@ -10,23 +10,23 @@ echo 'bundle install'
 bin/setup >/dev/null
 
 echo
-echo '----------'
-echo 'rubocop -D'
-rubocop -D
+echo '----------------------'
+echo 'bundle exec rubocop -D'
+bundle exec rubocop -D
 
 echo
-echo '-----------'
-echo 'sandi_meter'
-sandi_meter
+echo '-----------------------'
+echo 'bundle exec sandi_meter'
+bundle exec sandi_meter
 
 echo
-echo '------------'
-echo 'bundle-audit'
-bundle-audit
+echo '------------------------'
+echo 'bundle exec bundle-audit'
+bundle exec bundle-audit
 
-echo '----------------------------------------------'
-echo 'gemsurance --output log/gemsurance_report.html'
-gemsurance --output log/gemsurance_report.html
+echo '----------------------------------------------------------'
+echo 'bundle exec gemsurance --output log/gemsurance_report.html'
+bundle exec gemsurance --output log/gemsurance_report.html
 echo 'Gemsurance Report: log/gemsurance_report.html'
 
 echo '------------------------------------------------------------------------'
